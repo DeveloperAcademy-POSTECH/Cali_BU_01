ff6da7eee92a7a53ffac6edb0c8a95fb7ee91580
DK <dk143496@gmail.com>
2022-07-01 10:50:25 +0900

    delete: bring original files
    브랜치 작업을 위해 기존 버전으로 메인 변경

--- WordWorld/WordWorld/View/ContentView.swift
+++ WordWorld/WordWorld/View/ContentView.swift
@@ -8,34 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject var wordLoader = WordLoader()
+    
     
     var body: some View {
         
-//        TabView {
-//            MainView()
-//                .tabItem {
-//                    Label("Main", systemImage: "textformat.abc.dottedunderline")
-//                }
-//
-//            HistoryView()
-//                .tabItem {
-//                    Label("History", systemImage: "text.book.closed")
-//                }
-//        }
-        
         TabView {
-            MainView2()
+            MainView()
                 .tabItem {
                     Label("Main", systemImage: "textformat.abc.dottedunderline")
                 }
-                .environmentObject(wordLoader)
             
-            HistoryView()
-                .tabItem {
-                    Label("History", systemImage: "text.book.closed")
-                }
-                .environmentObject(wordLoader)
+            //            HistoryView()
+            //                .tabItem {
+            //                    Label("History", systemImage: "text.book.closed")
+            //                }
         }
     }
 }
